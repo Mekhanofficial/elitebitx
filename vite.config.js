604caad7eb0704e0e9f9167231c8922b8be3605f
mekhanofficial <prideofketa@gmail.com>
2025-05-22 05:06:48 +0100

    cod

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"; // Add this line
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true, // 👈 Add this
+    historyApiFallback: true,
   },
 });
